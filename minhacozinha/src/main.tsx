481268442b1f81a987157536e3972c18747cbf36
Jeff-Dev88 <74569799+Jeff-Dev88@users.noreply.github.com>
2022-10-20 22:13:47 -0300

    bug img

--- minhacozinha/src/main.tsx
+++ minhacozinha/src/main.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Nav from "./components/Nav/Nav";
+import Home from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <Nav />
-            <App />
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider>
+      <Nav />
+      <Home />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
---
+++ minhacozinha/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Stack, Image } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Stack>
+      <Image mt="100px" src="./images/cozinheiro.jpg" alt="cozinheiro" />
+    </Stack>
+  );
+}
