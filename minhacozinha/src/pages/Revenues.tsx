cdc25fd288e52c1f74fd93fdecf840819e393f20
Jeff-Dev88 <74569799+Jeff-Dev88@users.noreply.github.com>
2022-10-22 22:26:56 -0300

    Correções box, center e stack

--- minhacozinha/src/pages/Revenues.tsx
+++ minhacozinha/src/pages/Revenues.tsx
@@ -1,13 +1,13 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Center, Stack, Text } from "@chakra-ui/react";
 
 export default function Revenues() {
   return (
-    <Box w={500} h={1000} mt={100} id="pratos">
-      <Stack>
+    <Stack w={500} h={1000} mt={100} id="pratos">
+      <Center>
         <Text>
           <h2>Pratos</h2>
         </Text>
-      </Stack>
-    </Box>
+      </Center>
+    </Stack>
   );
 }
